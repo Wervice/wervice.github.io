bd27733f94ec1ac23eea39b6832aa5587970167e
wervice <108389772+Wervice@users.noreply.github.com>
2022-07-20 14:06:02 +0200

    Delete projects/Whitespace_pension_0 directory

--- projects/Whitespace_pension_0/SHA512.js
+++
@@ -1,5 +0,0 @@
-function sha512(str) { // Encode password
-    return crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str)).then(buf => {
-        return Array.prototype.map.call(new Uint8Array(buf), x => (('00' + x.toString(16)).slice(-2))).join('');
-    });
-}
